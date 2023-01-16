a0c774b228356be61d8651fa967d49c0827fe510
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-01-15 22:54:05 -0500

    finish

--- 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
+++ 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
@@ -24,10 +24,10 @@ $Promise.prototype._internalResolve = function(data){
     this._callHandlers();
 }};
 
-$Promise.prototype._internalReject = function (data){
+$Promise.prototype._internalReject = function (error){
     if (this._state === "pending"){
     this._state = "rejected";
-    this._value = data
+    this._value = error
     };
     this._callHandlers();
 };
@@ -36,25 +36,76 @@ $Promise.prototype._callHandlers = function () {
 
     while(this._handlerGroups.length){
         let uniqueHandler = this._handlerGroups.shift()
+
         if (this._state === "fulfilled"){
-            uniqueHandler.successCb && uniqueHandler.successCb(this._value);}
+            if (uniqueHandler.successCb){
+                try {
+                    const handleReturnValue = uniqueHandler.successCb(this._value);
+
+                        if (handleReturnValue instanceof $Promise){
+                                return handleReturnValue.then(
+                                   value => uniqueHandler.downstreamPromise._internalResolve(value),
+                                    reason => uniqueHandler.downstreamPromise._internalReject(reason)
+                                )
+                
+                        }
+                        else { uniqueHandler.downstreamPromise._internalResolve(handleReturnValue)       
+                        }
+                    }
+                catch (error) {
+                    uniqueHandler.downstreamPromise._internalReject(error)
+                              }
+                            }
+            else {
+                uniqueHandler.downstreamPromise._internalResolve(this._value)
+            }     
+            }
+
         else if (this._state === "rejected"){
-            uniqueHandler.errorCb && uniqueHandler.errorCb(this._value);}
+            if (uniqueHandler.errorCb){
+                try {
+                    const handleReturnValue = uniqueHandler.errorCb(this._value);
+
+                        if (handleReturnValue instanceof $Promise){
+
+                            if (handleReturnValue instanceof $Promise){
+                                return handleReturnValue.then(
+                                    value => uniqueHandler.downstreamPromise._internalResolve(value),
+                                    reason => uniqueHandler.downstreamPromise._internalReject(reason)
+                                )
+                        }
+                        }
+                        else { uniqueHandler.downstreamPromise._internalResolve(handleReturnValue)       
+                        }
+                    }
+                catch (error) {
+                    uniqueHandler.downstreamPromise._internalReject(error)
+                              }
+                            }
+            else {
+                uniqueHandler.downstreamPromise._internalReject(this._value)
+            }     
+            }
  }
 };
 
 $Promise.prototype.then = function ( successHandler,errorHandler) {
-    
+        const downstreamPromise = new $Promise ( () => {})
     this._handlerGroups.push({
         successCb: typeof successHandler === "function" &&  successHandler,
-        errorCb : typeof  errorHandler === "function" && errorHandler});
+        errorCb : typeof  errorHandler === "function" && errorHandler,
+        downstreamPromise,
+    });
+
+
 
     if (this._state !== "pending") this._callHandlers()
+    return downstreamPromise
 };
 
 $Promise.prototype.catch = function (successHandler,errorHandler) {
 
-    this.then(null, successHandler);
+   return this.then(null, successHandler);
 }
 
  
